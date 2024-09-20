e912722b42b2941ab7cc9277c0f090ba0bf58d36
LaineyCH <hen20028@byui.edu>
2024-09-20 13:57:09 +0400

    add activity 1 and 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,57 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        bool pass = true;
+        string letter = "";
+        string sign = "";
+        Console.Write("What is your grade percentage? (eg: 64) ");
+        string userInput = Console.ReadLine();
+        int grade = int.Parse(userInput);
+
+        if (grade >= 90)
+        {
+            letter = "A";
+        }
+        else if (grade >= 80)
+        {
+            letter = "B";
+        }
+        else if (grade >= 70)
+        {
+            letter = "C";
+        }
+        else if (grade >= 60)
+        {
+            letter = "D";
+            pass = false;
+        }
+        else
+        {
+            letter = "F";
+            pass = false;
+        }
+        
+        int rightMostDigit = grade % 10;
+        if (grade >= 60)
+        {
+            if (rightMostDigit >= 7 && grade <= 90)
+            {
+                sign = "+";
+            }
+            else if (rightMostDigit < 3)
+            {
+                sign = "-";
+            }
+        }
+        
+        Console.Write($"Your letter grade is {letter}{sign}, ");
+        if (pass)
+        {
+            Console.WriteLine($"you have passed the course. Congratulations!");
+        }
+        else
+        {
+            Console.WriteLine($"you have not passed the course this time, but please try again.");
+        }
     }
 }
