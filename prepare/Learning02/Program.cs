411c4a91227596cf91fce5db578ee9d889e820ab
LaineyCH <hen20028@byui.edu>
2024-09-22 22:20:55 +0400

    Adding w2 learning activity

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -1,9 +1,32 @@
 using System;
+using Learning02;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        // create instances of Job objects
+        Job job1 = new Job();
+        Job job2 = new Job();
+        // set member variables
+        job1._jobTitle = "Software Developer";
+        job1._company = "Apple";
+        job1._startYear = 2015;
+        job1._endYear = 2018;
+        job2._jobTitle = "Dev Ops";
+        job2._company = "Microsoft";
+        job2._startYear = 2018;
+        job2._endYear = 2021;
+        
+        // create an instance of Resume
+        Resume resume1 = new Resume();
+        // set member variables
+        resume1._name = "John Smith";
+        // add jobs
+        resume1._jobs.Add(job1);
+        resume1._jobs.Add(job2);
+        
+        // display the resume
+        resume1.Display();
     }
 }
